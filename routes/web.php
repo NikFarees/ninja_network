85d5397b804cdbeee4a4a4723cc770b5d8694f3b
Nik Farees <nikfarees2015@gmail.com>
2025-07-20 23:41:40 +0800

    #5 - Layouts & Slots

--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,18 @@ Route::get('/', function () {
 });
 
 Route::get('/ninjas', function () {
-  $ninjas = [
-    ["name" => "mario","skill" => 75, "id" => "1"],
-    ["name" => "luigi","skill" => 45, "id" => "2"],
-  ];
+    $ninjas = [
+        ["name" => "mario", "skill" => 75, "id" => "1"],
+        ["name" => "luigi", "skill" => 45, "id" => "2"],
+    ];
 
-  return view('ninjas.index', ["greeting" => "hello", "ninjas" => $ninjas]);
+    return view('ninjas.index', ["greeting" => "hello", "ninjas" => $ninjas]);
+});
+
+Route::get('/ninjas/create', function () {
+    return view('ninjas.create');
 });
 
 Route::get('/ninjas/{id}', function ($id) {
-  // do something with the id
-  // e.g. fetch a record from the db
-
-  return view('ninjas.show', ['id' => $id]);
+    return view('ninjas.show', ['id' => $id]);
 });
