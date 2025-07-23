70a7069a153636a157bef9184d5e3cf7c1722cc4
Nik Farees <nikfarees2015@gmail.com>
2025-07-23 12:33:54 +0800

    #26 - Registering New Users

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -16,8 +18,19 @@ class AuthController extends Controller
         return view('auth.login');
     }
 
-    public function register()
+    public function register(Request $request)
     {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create($validated);
+
+        Auth::login($user); // Automatically log in the user after registration
+
+        return redirect()->route('ninjas.index');
     }
 
     public function login()
